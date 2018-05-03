ef5be8ca3b31d200170c99cba016f007ba0a1aa5
Astemir Eleev <astemireleev@gmail.com>
2018-05-03 13:31:53 +0300

    Refactored: added support for failable initialiser

--- ios-spritekit-flappy-flying-bird/Adapeters/GameSceneAdapter.swift
+++ ios-spritekit-flappy-flying-bird/Adapeters/GameSceneAdapter.swift
@@ -12,6 +12,14 @@ class GameSceneAdapter: GameSceneProtocol {
     
     // MARK: - Properties
     
+    let gravity: CGFloat = -3.0
+    let playerSize = CGSize(width: 100, height: 100)
+    let backgroundResourceName = "Background-Winter"
+    let playerResourceName = "Bird Right"
+    let floorDistance: CGFloat = 70
+    
+    // MARK: - Conformance to GameSceneProtocol
+    
     weak var scene: SKScene?
     
     var updatables = [Updatable]()
@@ -19,27 +27,25 @@ class GameSceneAdapter: GameSceneProtocol {
     
     // MARK: - Initializers
     
-    required init(with scene: SKScene) {
+    required init?(with scene: SKScene) {
         self.scene = scene
         
         guard let scene = self.scene else {
-            return
+            debugPrint(#function + " could not unwrap the host SKScene instance")
+            return nil
         }
+        
         prepareWorld(for: scene)
         prepareInfiniteBackgroundScroller(for: scene)
-      
         preparePlayer(for: scene)
     }
 
     // MARK: - Helpers
     
     private func prepareWorld(for scene: SKScene) {
-        let floorDistance: CGFloat = 70
-        
-        scene.physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
+        scene.physicsWorld.gravity = CGVector(dx: 0.0, dy: gravity)
         let rect = CGRect(x: 0, y: floorDistance, width: scene.size.width, height: scene.size.height - floorDistance)
         scene.physicsBody = SKPhysicsBody(edgeLoopFrom: rect)
-        
         
         let boundary: PhysicsCategories = .boundary
         let player: PhysicsCategories = .player
@@ -49,8 +55,7 @@ class GameSceneAdapter: GameSceneProtocol {
     }
     
     private func preparePlayer(for scene: SKScene) {
-        let birdSize = CGSize(width: 100, height: 100)
-        let bird = BirdNode(animationTimeInterval: 0.1, withTextureAtlas: "Bird Right", size: birdSize)
+        let bird = BirdNode(animationTimeInterval: 0.1, withTextureAtlas: playerResourceName, size: playerSize)
         bird.position = CGPoint(x: bird.size.width / 2 + 24, y: scene.size.height / 2)
         bird.zPosition = 10
         
@@ -61,7 +66,7 @@ class GameSceneAdapter: GameSceneProtocol {
     }
     
     private func prepareInfiniteBackgroundScroller(for scene: SKScene) {
-        let infiniteBackgroundNode = InfiniteSpriteScrollNode(fileName: "Background-Winter", scaleFactor: CGPoint(x: 2.98, y: 2.98))
+        let infiniteBackgroundNode = InfiniteSpriteScrollNode(fileName: backgroundResourceName, scaleFactor: CGPoint(x: 2.98, y: 2.98))
         infiniteBackgroundNode.zPosition = 0
         
         scene.addChild(infiniteBackgroundNode)
