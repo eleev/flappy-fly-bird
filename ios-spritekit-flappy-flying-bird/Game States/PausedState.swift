c8b28efad9dc2f8651ef2bf56d93be8a6267c185
Astemir Eleev <astemireleev@gmail.com>
2018-05-09 22:42:33 +0300

    Implemented PauseState class

--- ios-spritekit-flappy-flying-bird/Game States/PausedState.swift
+++ ios-spritekit-flappy-flying-bird/Game States/PausedState.swift
@@ -11,4 +11,49 @@ import SpriteKit
 
 class PausedState: GKState {
     
+    // MARK: - Properites
+    
+    var overlaySceneFileName: String {
+        return "PauseScene"
+    }
+    
+    unowned var levelScene: SKScene
+    unowned var adapter: GameSceneAdapter
+    var overlay: SceneOverlay!
+    
+    // MARK: - Intializers
+    
+    init(scene: SKScene, adapter: GameSceneAdapter) {
+        self.levelScene = scene
+        self.adapter = adapter
+        super.init()
+        // Look up for the pause button in order to be able to hide it when the state changes back and forth
+        overlay = SceneOverlay(overlaySceneFileName: overlaySceneFileName, zPosition: 1000)
+    }
+    
+    // MARK: GKState Life Cycle
+    
+    override func didEnter(from previousState: GKState?) {
+        super.didEnter(from: previousState)
+        
+        levelScene.isPaused = true
+        adapter.overlay = overlay
+    }
+    
+    override func willExit(to nextState: GKState) {
+        super.willExit(to: nextState)
+        
+        levelScene.isPaused = false
+        adapter.overlay = nil
+    }
+    
+    // MARK: Convenience
+    
+    func button(withIdentifier identifier: ButtonIdentifier) -> ButtonNode? {
+        return overlay.contentNode.childNode(withName: "//\(identifier.rawValue)") as? ButtonNode
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+        return true
+    }
 }
