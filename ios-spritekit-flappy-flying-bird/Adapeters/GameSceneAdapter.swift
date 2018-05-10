6b84a338f57a0d51818ebc6fcf64029e0af037ff
Astemir Eleev <astemireleev@gmail.com>
2018-05-10 16:07:35 +0300

    Refactored the implementation: moved some functionality into corresponding State Machine states

--- ios-spritekit-flappy-flying-bird/Adapeters/GameSceneAdapter.swift
+++ ios-spritekit-flappy-flying-bird/Adapeters/GameSceneAdapter.swift
@@ -23,17 +23,19 @@ class GameSceneAdapter: NSObject, GameSceneProtocol {
     
     // MARK: - Properties
     
-    let gravity: CGFloat = -3.0
+    let gravity: CGFloat = -5.0
     let playerSize = CGSize(width: 100, height: 100)
     let backgroundResourceName = "Background-Winter"
     let playerResourceName = "Bird Right"
     let floorDistance: CGFloat = 0
     
-    private(set) var score: Int = 0
+    var score: Int = 0
     private(set) var scoreLabel: SKLabelNode?
     
     private(set) var scoreSound = SKAction.playSoundFileNamed("Coin.wav", waitForCompletion: false)
     private(set) var hitSound = SKAction.playSoundFileNamed("Hit_Hurt.wav", waitForCompletion: false)
+    
+    var bird: BirdNode?
     
     private(set) lazy var menuAudio: SKAudioNode = {
         let audioNode = SKAudioNode(fileNamed: "POL-catch-them-all-short.wav")
@@ -74,21 +76,39 @@ class GameSceneAdapter: NSObject, GameSceneProtocol {
                 // Animate the overlay in.
                 overlay.backgroundNode.alpha = 1.0
                 overlay.backgroundNode.run(SKAction.fadeIn(withDuration: 0.25))
-//                overlay.updateScale()
                 
                 buttons = scene.findAllButtonsInScene()
             }
             
             // Animate the old overlay out.
             oldValue?.backgroundNode.run(SKAction.fadeOut(withDuration: 0.25)) {
+                debugPrint(#function + " remove old overlay")
                 oldValue?.backgroundNode.removeFromParent()
             }
         }
     }
     
+    private var _isHUDHidden: Bool = false
+    var isHUDHidden: Bool {
+        get {
+            return _isHUDHidden
+        }
+        set(newValue) {
+            _isHUDHidden = newValue
+            
+            if let world = self.scene?.childNode(withName: "world") {
+                world.childNode(withName: "Score Node")?.isHidden = newValue
+                world.childNode(withName: "Pause")?.isHidden = newValue
+            }
+        }
+    }
+
+    
     // MARK: - Private properties
     
     private(set) var infiniteBackgroundNode: InfiniteSpriteScrollNode?
+    private let notification = UINotificationFeedbackGenerator()
+    private let impact = UIImpactFeedbackGenerator(style: .heavy)
     
     // MARK: - Initializers
     
@@ -110,14 +130,6 @@ class GameSceneAdapter: NSObject, GameSceneProtocol {
         
         prepareWorld(for: scene)
         prepareInfiniteBackgroundScroller(for: scene)
-        preparePlayer(for: scene)
-        launchPipeFactory(for: scene)
-        
-        // Game state - Playing
-        SKAction.play()
-        scene.addChild(playingAudio)
-        
-        stateMahcine?.enter(PlayingState.self)
     }
     
     convenience init?(with scene: SKScene, stateMachine: GKStateMachine) {
@@ -141,80 +153,12 @@ class GameSceneAdapter: NSObject, GameSceneProtocol {
         scene.physicsWorld.contactDelegate = self
     }
     
-    private func preparePlayer(for scene: SKScene) {
-        let bird = BirdNode(animationTimeInterval: 0.1, withTextureAtlas: playerResourceName, size: playerSize)
-        bird.position = CGPoint(x: bird.size.width / 2 + 24, y: scene.size.height / 2)
-        bird.zPosition = 10
-        
-        scene.addChild(bird)
-        
-        updatables.append(bird)
-        touchables.append(bird)
-    }
-    
     private func prepareInfiniteBackgroundScroller(for scene: SKScene) {
         infiniteBackgroundNode = InfiniteSpriteScrollNode(fileName: backgroundResourceName, scaleFactor: CGPoint(x: 2.98, y: 2.98))
         infiniteBackgroundNode!.zPosition = 0
         
         scene.addChild(infiniteBackgroundNode!)
         updatables.append(infiniteBackgroundNode!)
-    }
-    
-    private func launchPipeFactory(for scene: SKScene) {
-        
-        let topPipeName = "top-pipe"
-        let bottomPipeName = "bottom-pipe"
-        let thresholdPipeName = "threshold-pipe"
-        
-        let cleanUpBottomPipeAction = SKAction.run { [weak self] in
-            self?.infiniteBackgroundNode?.childNode(withName: bottomPipeName)?.removeFromParent()
-        }
-        let cleanUpTopPipeAction = SKAction.run { [weak self] in
-            self?.infiniteBackgroundNode?.childNode(withName: topPipeName)?.removeFromParent()
-        }
-        let cleanUpThresholdPipeAction = SKAction.run { [weak self] in
-            self?.infiniteBackgroundNode?.childNode(withName: thresholdPipeName)?.removeFromParent()
-        }
-        
-        let waitAction = SKAction.wait(forDuration: 3.0)
-        
-        let pipeMoveDuration: TimeInterval = 4.0
-        
-        let producePipeAction = SKAction.run { [weak self] in
-            
-            guard let pipes = PipeFactory.produce(sceneSize: scene.size) else {
-                return
-            }
-            let scrollingNode = self?.infiniteBackgroundNode
-            pipes.bottom.name = bottomPipeName
-            pipes.top.name = topPipeName
-            pipes.threshold.name = thresholdPipeName
-            
-            scrollingNode?.addChild(pipes.top)
-            scrollingNode?.addChild(pipes.bottom)
-            scrollingNode?.addChild(pipes.threshold)
-            
-            // Construct move actions for pipes
-            let bottom = pipes.bottom
-            let top = pipes.top
-            let threshold = pipes.threshold
-            
-            let pipeBottomMoveAction = SKAction.move(to: CGPoint(x: -bottom.size.width, y: bottom.position.y), duration: pipeMoveDuration)
-            let pipeTopMoveAction = SKAction.move(to: CGPoint(x: -top.size.width, y: top.position.y), duration: pipeMoveDuration)
-            let pipeThresholdMoveAction = SKAction.move(to: CGPoint(x: -threshold.size.width, y: threshold.position.y), duration: pipeMoveDuration - 0.4)
-            
-            let pipeBottomMoveSequence = SKAction.sequence([pipeBottomMoveAction, cleanUpBottomPipeAction])
-            let pipeTopMoveSequence = SKAction.sequence([pipeTopMoveAction, cleanUpTopPipeAction])
-            let pipeThresholdMoveSequence = SKAction.sequence([pipeThresholdMoveAction, cleanUpThresholdPipeAction])
-            
-            bottom.run(pipeBottomMoveSequence)
-            top.run(pipeTopMoveSequence)
-            threshold.run(pipeThresholdMoveSequence)
-        }
-        
-        let sequenceAction = SKAction.sequence([waitAction, producePipeAction])
-        let infinitePipeProducer = SKAction.repeatForever(sequenceAction)
-        scene.run(infinitePipeProducer)
     }
     
 }
@@ -230,11 +174,11 @@ extension GameSceneAdapter: SKPhysicsContactDelegate {
             score += 1
             scoreLabel?.text = "Score \(score)"
             scene?.run(scoreSound)
+            notification.notificationOccurred(.success)
         }
         
         if collision == (player | PhysicsCategories.pipe.rawValue) {
             // game over state, the player has touched pipe
-//            scoreLabel?.text = "Dead by Pipe"
             deadState()
             hit()
         }
@@ -242,24 +186,24 @@ extension GameSceneAdapter: SKPhysicsContactDelegate {
         if collision == (player | PhysicsCategories.boundary.rawValue) {
             // game over state, the player has touched the boundary of the world (floor)
             // player's position needs to be set to the default one
-//            scoreLabel?.text = "Deap by Falling"
             deadState()
             hit()
         }
 
     }
- 
+    
+    // MARK: - Collision Helpers
+    
     func deadState() {
-        if let playingAudioNodeName = playingAudio.name {
-            scene?.childNode(withName: playingAudioNodeName)?.removeFromParent()
+        // Do not enter the same state twice
+        if stateMahcine?.currentState is GameOverState {
+            return
         }
-        if scene?.childNode(withName: menuAudio.name!) == nil {
-            scene?.addChild(menuAudio)
-            SKAction.play()
-        }
+        stateMahcine?.enter(GameOverState.self)
     }
     
     func hit() {
+        impact.impactOccurred()
         scene?.run(hitSound)
     }
     
