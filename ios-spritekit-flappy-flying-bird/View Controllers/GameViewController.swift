bcd51cfc6718c7cdd96028c437acfaaa98e70b93
Astemir Eleev <astemireleev@gmail.com>
2018-05-19 12:52:15 +0300

    Implemented convenience Scenes and NodeScale enums

--- ios-spritekit-flappy-flying-bird/View Controllers/GameViewController.swift
+++ ios-spritekit-flappy-flying-bird/View Controllers/GameViewController.swift
@@ -10,12 +10,59 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+enum Scenes: String {
+    case title = "TitleScene"
+    case game = "GameScene"
+    case setting = "SettingsScene"
+    case score = "ScoreScene"
+    case pause = "PauseScene"
+    case failed = "FailedScene"
+}
+
+extension Scenes {
+    func getName() -> String {
+        let padId = " iPad"
+        let isPad = UIDevice.current.userInterfaceIdiom == .pad
+        
+        return isPad ? self.rawValue + padId : self.rawValue
+    }
+}
+
+enum NodeScale: Float {
+    case gameBackgroundScale
+}
+
+extension NodeScale {
+    
+    func getValue() -> Float {
+        let isPad = UIDevice.current.userInterfaceIdiom == .pad
+        
+        switch self {
+        case .gameBackgroundScale:
+            return isPad ? 1.5 : 1.25
+        }
+    }
+}
+
+extension CGPoint {
+    init(x: Float, y: Float) {
+        self.init()
+        self.x = CGFloat(x)
+        self.y = CGFloat(y)
+    }
+}
+
+
 class GameViewController: UIViewController {
 
+    // MARK: - Overrides
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let scene = SKScene(fileNamed: "TitleScene") as? TitleScene {
+        let sceneName = Scenes.title.getName()
+        
+        if let scene = SKScene(fileNamed: sceneName) as? TitleScene {
 
             // Set the scale mode to scale to fit the window
             scene.scaleMode = .aspectFit
