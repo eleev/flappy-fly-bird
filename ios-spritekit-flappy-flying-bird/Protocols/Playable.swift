51c8473a9570c48c3f9c52de55f520e3a57fb4c8
Astemir Eleev <astemireleev@gmail.com>
2018-05-22 15:55:16 +0300

    Created Playable protocol

---
+++ ios-spritekit-flappy-flying-bird/Protocols/Playable.swift
@@ -0,0 +1,14 @@
+//
+//  Playable.swift
+//  ios-spritekit-flappy-flying-bird
+//
+//  Created by Astemir Eleev on 22/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+// TODO: Requires implementation
+protocol Playable {
+    
+}
