4c0ac771ae1f20061915c47b083e2a88b99cc228
Astemir Eleev <astemireleev@gmail.com>
2018-05-27 21:37:21 +0300

    Implemented full support for SKTexture+GIF as extension

--- ios-spritekit-flappy-flying-bird/Extensions/SKSpriteNode+GIF.swift
+++ ios-spritekit-flappy-flying-bird/Extensions/SKSpriteNode+GIF.swift
@@ -11,38 +11,65 @@ import ImageIO
 
 extension SKSpriteNode {
     
-    func animateWithLocalGIF(named name: String){
+    convenience init(withAnimatedGif name: String, correctAspectRatioFor width: CGFloat) {
+        self.init(texture: nil, color: .clear, size: .zero)
+        animateWithLocalAspectCorrectGIF(named: name, width: width)
+    }
+    
+    func animateWithLocalAspectCorrectGIF(named name: String, width: CGFloat) {
+        guard let size = animateWithLocalGIF(named: name) else {
+            debugPrint(#function + " could not unwrap size of the GIF texture - the method will be aborted")
+            return
+        }
+        
+        let aspect = size.width / size.height
+        let newHeight = size.width / aspect
+        self.size.width = size.width
+        self.size.height = newHeight
+    }
+    
+    @discardableResult
+    func animateWithLocalGIF(named name: String) -> CGSize? {
         
         guard let bundleURL = Bundle.main.url(forResource: name, withExtension: "gif") else {
             debugPrint(#function + " image with .gif extension does not exist")
-                return
+                return nil
         }
         
         do {
             let imageData = try Data(contentsOf: bundleURL)
-
-            if let textures = SKSpriteNode.gif(with: imageData as NSData) {
-                let action = SKAction.repeatForever(SKAction.animate(with: textures, timePerFrame: 0.1))
-                self.run(action)
+            let data =  SKSpriteNode.gif(with: imageData as NSData)
+            
+            guard let textures = data.0 else  {
+                return nil
             }
+            
+            debugPrint("textures: " , textures)
+            debugPrint("delays: " , data.1)
+            
+            let action = SKAction.repeatForever(SKAction.animate(with: textures, timePerFrame: data.1[0]))
+            self.run(action)
+            
+            return textures.first?.size()
         } catch {
             debugPrint(#function + " could not create data source from bundle URL: ", error)
+            return nil
         }
     }
     
-    public class func gif(with data: NSData) -> [SKTexture]? {
+    class func gif(with data: NSData) -> ([SKTexture]?, [Double]) {
         // Create source from data
         guard let source = CGImageSourceCreateWithData(data, nil) else {
             print("SwiftGif: Source for the image does not exist")
-            return nil
+            return (nil, [])
         }
         
         return SKSpriteNode.animatedImage(with: source)
     }
     
-    class func animatedImage(with source: CGImageSource) -> [SKTexture]? {
+    class func animatedImage(with source: CGImageSource) -> ([SKTexture]?, [Double]) {
         let count = CGImageSourceGetCount(source)
-        var delays = [Int]()
+        var delays = [Double]()
         var textures = [SKTexture]()
         
         for i in 0..<count {
@@ -53,21 +80,18 @@ extension SKSpriteNode {
             }
         
             let delaySeconds = SKSpriteNode.delayForImageAtIndex(index: Int(i), source: source)
-            delays.append(Int(delaySeconds * 1000.0))
+            delays.append(delaySeconds)
         }
         
-        return textures
+        return (textures, delays)
     }
     
     class func gcdForArray(array: Array<Int>) -> Int {
-        if array.isEmpty {
-            return 1
-        }
-        
+        if array.isEmpty { return 1 }
         var gcd = array[0]
         
-        for val in array {
-            gcd = SKSpriteNode.gcdForPair(a: val, b: gcd)
+        array.forEach { val in
+            gcd = SKSpriteNode.gcdFor(pair: val, target: gcd)
         }
         
         return gcd
@@ -76,35 +100,28 @@ extension SKSpriteNode {
     class func delayForImageAtIndex(index: Int, source: CGImageSource!) -> Double {
         var delay = 0.1
         
-        // Get dictionaries
         let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
-        let unmanaged = Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()
-        let gifProperties: CFDictionary = unsafeBitCast(CFDictionaryGetValue(cfProperties, unmanaged), to: CFDictionary.self)
+        let gifProperties: CFDictionary = unsafeBitCast(CFDictionaryGetValue(cfProperties, Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()), to: CFDictionary.self)
         
-        
-        
-        // Get delay time
-        let unmanagedUnclampedDelay = Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()
-        var delayObject: AnyObject = unsafeBitCast(CFDictionaryGetValue(gifProperties, unmanagedUnclampedDelay), to: AnyObject.self)
+        var delayObject: AnyObject = unsafeBitCast(CFDictionaryGetValue(gifProperties, Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()), to: AnyObject.self)
         
         if delayObject.doubleValue == 0 {
-            let unmanagedDelayTime = Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()
-            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties, unmanagedDelayTime), to: AnyObject.self)
+            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties, Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()), to: AnyObject.self)
         }
         
         delay = delayObject as! Double
         
-        if delay < 0.1 {
-            delay = 0.1 // Make sure they're not too fast
+        if delay < 0.01 {
+            delay = 0.01
         }
-        
+
         return delay
     }
     
-    class func gcdForPair(a: Int, b: Int) -> Int {
-        var a = a
-        var b = b
-        
+    class func gcdFor(pair value: Int, target: Int) -> Int {
+        var a = value
+        var b = target
+
         // Swap for modulo
         if a < b {
             let c = a
@@ -114,12 +131,12 @@ extension SKSpriteNode {
         
         // Get greatest common divisor
         var rest: Int
+        
         while true {
             rest = a % b
             
-            if rest == 0 {
-                return b
-            } else {
+            if rest == 0 { return b }
+            else {
                 a = b
                 b = rest
             }
